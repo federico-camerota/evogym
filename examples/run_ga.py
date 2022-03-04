ea533fec45984c540ea33e89f31589d5ddb1e254
federicopigozzi <pigozzife@gmail.com>
2022-03-04 11:53:38 +0100

    exp params

--- examples/run_ga.py
+++ examples/run_ga.py
@@ -1,18 +1,23 @@
 import random
 import numpy as np
+import sys
 
 from ga.run import run_ga
 
+from abc_sr.evogym_utils import get_number_evaluations, get_robot_shape
+
 if __name__ == "__main__":
-    seed = 0
+    seed = int(sys.argv[2])
+    env = sys.argv[4]
+    task = sys.argv[4].split("-")[0].lower()
     random.seed(seed)
     np.random.seed(seed)
 
     run_ga(
-        pop_size=3,
-        structure_shape=(3, 3),
-        experiment_name="walk_test",
-        max_evaluations=6,
-        train_iters=50,
-        num_cores=4
+        pop_size=25,
+        structure_shape=get_robot_shape(env),
+        experiment_name="-".join([task, str(seed)]),
+        max_evaluations=get_number_evaluations(env),
+        train_iters=1000,
+        num_cores=25
     )
